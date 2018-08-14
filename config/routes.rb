a33497222da39e6408f39474ed3af2d3018824e8
Wolferinesda <drhalterman1@yahoo.com>
2018-08-14 15:50:09 -0400

    In progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'advertisements/index'
-  get 'advertisements/show'
-  get 'advertisements/new'
-  get 'advertisements/create'
+  resources :advertisements
   resources :posts
   get 'about' => 'welcome#about'
   get 'welcome/contact'
