c9e8364c5e8c1d747edb1230483d11341d5a72cf
Wolferinesda <drhalterman1@yahoo.com>
2018-09-09 09:53:38 -0400

    replaced code to simplify it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(user_params)
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
@@ -20,11 +16,7 @@ class UsersController < ApplicationController
   end
 
   def confirm
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(user_params)
   end
 
   def user_params
