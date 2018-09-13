90e100d2dce1446e51bbade8106f644e4df832cf
Wolferinesda <drhalterman1@yahoo.com>
2018-09-13 13:18:10 -0400

    Error with favorite mailing

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,15 +3,9 @@ class FavoriteMailer < ApplicationMailer
 
   def new_comment(user, post, comment)
 
-<<<<<<< HEAD
     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
     headers["References"] = "<post/#{post.id}@your-app-name.example>"
-=======
-    headers["Message-ID"] = "<comments/#{comment.id}@bloccit.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
-    headers["References"] = "<post/#{post.id}@bloccit.example>"
->>>>>>> 556c984e17717e83914b19fa1fced9a4aeb0e9b5
 
     @user = user
     @post = post
@@ -21,7 +15,6 @@ class FavoriteMailer < ApplicationMailer
   end
 
   def new_post(post)
-<<<<<<< HEAD
     headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
     headers["References"] = "<post/#{post.id}@your-app-name.example>"
@@ -29,14 +22,5 @@ class FavoriteMailer < ApplicationMailer
     @post = post
 
     mail(to: post.user.email, subject: "You're following, #{post.title}")
-=======
-    headers["Message-ID"] = "<post/#{post.id}@bloccit.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@bloccit.example>"
-    headers["References"] = "<post/#{post.id}@bloccit.example>"
-
-    @post = post
-
-    mail(to: post.user.email, subject: "You're following, #{post.title}!")
->>>>>>> 556c984e17717e83914b19fa1fced9a4aeb0e9b5
   end
 end
